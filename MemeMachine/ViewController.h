147dd34087c9e00da845926a6ec49fade7b21b31
nikolai <nikolai@nznlabs.com>
2015-10-01 19:09:42 -0700

    Initial Commit

---
+++ MemeMachine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemeMachine
+//
+//  Created by Nikolai on 10/1/15.
+//  Copyright (c) 2015 DigitalArsenal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MemeMachine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MemeMachine
+//
+//  Created by Nikolai on 10/1/15.
+//  Copyright (c) 2015 DigitalArsenal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
